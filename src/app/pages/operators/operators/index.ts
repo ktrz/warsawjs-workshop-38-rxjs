6148a8c454efd078c9cd61069f33548dfff69a4f
Chris Trześniewski <krzysztof.trzesniewski@scalac.io>
2019-11-24 12:20:51 +0100

    * fixed build

--- src/app/pages/operators/operators/index.ts
+++ src/app/pages/operators/operators/index.ts
@@ -1,5 +1,3 @@
-export * from './buffer-delay.operator';
 export * from './custom-map1.operator';
 export * from './custom-map2.operator';
 export * from './not-null.operator';
-export * from './switch-case.operator';
